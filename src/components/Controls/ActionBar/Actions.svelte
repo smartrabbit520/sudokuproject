dd2c24b3e01c024299e1b4de70d0c26180c98ce4
Xinqi Zeng <2284182343@qq.com>
2024-12-17 14:30:55 +0800

    实现撤回和重做

--- src/components/Controls/ActionBar/Actions.svelte
+++ src/components/Controls/ActionBar/Actions.svelte
@@ -19,17 +19,39 @@
 			userGrid.applyHint($cursor);
 		}
 	}
+
+	let isUndoable;
+	let isRedoable;
+
+	userGrid.isUndoable.subscribe(value => {
+		isUndoable = value;
+	});
+
+	userGrid.isRedoable.subscribe(value => {
+		isRedoable = value;
+	});
+
+	function handleUndo() {
+        console.log('Undo button clicked');
+		userGrid.undo();
+    }
+
+	function handleRedo() {
+        console.log('Redo button clicked');
+		userGrid.redo();
+    }
+
 </script>
 
 <div class="action-buttons space-x-3">
 
-	<button class="btn btn-round" disabled={$gamePaused} title="Undo">
+	<button class="btn btn-round" disabled={$gamePaused || !isUndoable} title="Undo" on:click={handleUndo}>
 		<svg class="icon-outline" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 			<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 10h10a8 8 0 018 8v2M3 10l6 6m-6-6l6-6" />
 		</svg>
 	</button>
 
-	<button class="btn btn-round" disabled={$gamePaused} title="Redo">
+	<button class="btn btn-round" disabled={$gamePaused || !isRedoable} title="Redo" on:click={handleRedo}>
 		<svg class="icon-outline" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 			<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 10h-10a8 8 90 00-8 8v2M21 10l-6 6m6-6l-6-6" />
 		</svg>
